712274c6a2e2cc944057d4c82616d35597db5c1f
wabasit <basit.wahab@amalitechtraining.org>
2025-06-24 13:53:15 +0000

    Add ensure_table_exists function to create DynamoDB tables if they do not exist

--- scripts/load_dynamodb.py
+++ scripts/load_dynamodb.py
@@ -32,3 +32,28 @@ tables_config = {
     }
 }
 
+# Create table if it does not exist
+def ensure_table_exists(table_name, hash_key, range_key=None):
+    existing_tables = dynamodb_client.list_tables()["TableNames"]
+    if table_name in existing_tables:
+        print(f"✔ Table '{table_name}' exists.")
+        return
+
+    key_schema = [{"AttributeName": hash_key[0], "KeyType": "HASH"}]
+    attr_definitions = [{"AttributeName": hash_key[0], "AttributeType": hash_key[1]}]
+
+    if range_key:
+        key_schema.append({"AttributeName": range_key[0], "KeyType": "RANGE"})
+        attr_definitions.append({"AttributeName": range_key[0], "AttributeType": range_key[1]})
+
+    print(f" Creating table '{table_name}'...")
+    dynamodb_client.create_table(
+        TableName=table_name,
+        KeySchema=key_schema,
+        AttributeDefinitions=attr_definitions,
+        ProvisionedThroughput={"ReadCapacityUnits": 5, "WriteCapacityUnits": 5}
+    )
+    waiter = dynamodb_client.get_waiter("table_exists")
+    waiter.wait(TableName=table_name)
+    print(f"Table '{table_name}' created.")
+
