2dd746b8699b91f597d75a4a86561f4c4b4cf37a
wabasit <basit.wahab@amalitechtraining.org>
2025-06-24 13:57:17 +0000

    Adding a dag for music data pipeline orchestration

---
+++ dags/streaming_pipeline_dag.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+from airflow.models import Variable
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
+from airflow.providers.amazon.aws.operators.sns import SnsPublishOperator
+from datetime import datetime
+import boto3
+import time
+from datetime import timedelta
+
+# AWS Clients
+region = "eu-north-1"
+glue = boto3.client("glue", region_name=region)
+s3 = boto3.resource("s3", region_name=region)
+
+# Variables
+glue_job_name = Variable.get("GLUE_JOB_NAME")
+validation_job = "val_glue_job"
+dynamodb_job_name = "load_dynamodb"
+bucket = Variable.get("S3_BUCKET")
+streaming_prefix = Variable.get("S3_STREAMING_PREFIX").replace("s3://project3dt/", "")
+sns_arn = Variable.get("SNS_TOPIC_ARN")
+
+default_args = {
+    'owner': 'airflow',
+    'retries': 1,
+}
+
+with DAG(
+    dag_id="music_data_pipeline",
+    start_date=days_ago(1),
+    schedule_interval=timedelta(minutes=15),  # Every 5 minutes
+    catchup=False,
+    default_args=default_args
+) as dag:
+
+    
